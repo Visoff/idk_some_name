d006611f12f1c28c97406003a3a43af558b407b7
Ilya <ikalinin01@mail.ru>
2023-08-04 17:13:49 +0600

    tests for golang ???

--- golang_server/api/main.go
+++ golang_server/api/main.go
@@ -8,6 +8,10 @@ import (
 var Mux *http.ServeMux = http.NewServeMux()
 
 func Init() {
+	Mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("pong"))
+	})
 	ApplyMessageHandlers()
 	ApplyUserHandlers()
 	ApplyChatHandlers()
