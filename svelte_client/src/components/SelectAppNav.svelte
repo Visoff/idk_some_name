0fd1a2711c8eeba0f441915e918b2f65970fc8be
Ilya <ikalinin01@mail.ru>
2023-08-13 18:44:28 +0600

    ...

--- svelte_client/src/components/SelectAppNav.svelte
+++ svelte_client/src/components/SelectAppNav.svelte
@@ -1,8 +1,5 @@
 <script>
 	import App from "./SelectAppNav/App.svelte";
-    import {clerk, clerk_ready} from "$lib"
-    $: profile_pic = $clerk_ready ? $clerk.user?.imageUrl : "#"
-    $: home_url = $clerk_ready ? $clerk.buildUserProfileUrl() : "/"
 </script>
 <div class="
     w-[5rem] p-2 gap-2 bg-blue-100
@@ -13,9 +10,9 @@
         aspect-square w-full p-2 flex
     "
     >
-        <a href={home_url} class="
+        <a href="/" class="
             flex-1 border-blue-700 border rounded-full flex
-        "><img src={profile_pic} alt="" class="flex-1 rounded-full"></a>
+        "><img src="#" alt="" class="flex-1 rounded-full"></a>
     </div>
     <div class="
         w-full h-px bg-black
