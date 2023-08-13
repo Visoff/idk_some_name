b1a21fcf66eeaff7176a909f2f5e0ad5586c5514
Ilya <ikalinin01@mail.ru>
2023-08-13 18:43:10 +0600

    ...

--- svelte_client/src/components/Layout.svelte
+++ svelte_client/src/components/Layout.svelte
@@ -1,36 +1,7 @@
 <script>
 	import { browser } from "$app/environment";
 	import SelectAppNav from "./SelectAppNav.svelte";
-    import {clerk as clerk_import, clerk_ready} from "$lib"
-	import { host } from "$lib/env";
-
-    let signOut = () => {}
-    
-    const clerk = $clerk_import
     if (browser) {
-        (async () => {
-            await clerk.load({
-                "afterSignInUrl":"http://localhost:5173",
-                "afterSignUpUrl":"http://localhost:5173",
-            })
-            if (clerk.user == undefined) {
-                clerk.redirectToSignIn()
-                return
-            }
-            if (localStorage.getItem("user_token") == undefined) {
-                fetch(host+"/user/sign/", {
-                    method:"POST",
-                    body:clerk.user.id
-                }).then(response => response.text())
-                .then(token => {
-                    localStorage.setItem("user_token", token)
-                })
-                .catch(console.error)
-            }
-            // @ts-ignore
-            window.signOut = clerk.signOut
-            $clerk_ready = true
-        })().catch(() => undefined)
     }
 </script>
 <div class="absolute top-0 left-0 w-screen h-screen flex overflow-hidden">
--- svelte_client/src/lib/index.ts
+++ svelte_client/src/lib/index.ts
@@ -1,8 +1,4 @@
-import Clerk from "@clerk/clerk-js";
 import { writable, type Writable } from "svelte/store";
 
 export let CurrentChat:Writable<string/*uuid*/|false> = writable(false)
 export let CurrentApp:Writable<"Chat"|"Video"> = writable("Chat")
-
-export let clerk:Writable<Clerk> = writable(new Clerk("pk_test_Y2hhbXBpb24ta2luZ2Zpc2gtMTguY2xlcmsuYWNjb3VudHMuZGV2JA"))
-export let clerk_ready:Writable<boolean> = writable(false)
