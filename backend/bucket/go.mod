0dcf901ba91489229e8a46cd6a797dc9b0efa594
Ilya <ikalinin01@mail.ru>
2023-09-09 13:34:27 +0600

    bucket

---
+++ backend/bucket/go.mod
@@ -0,0 +1,7 @@
+module backend/bucket
+
+go 1.20
+
+require github.com/Visoff/idk_some_name/golang_library v0.0.0-20230909064049-247a7e9df271
+
+require github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
---
+++ backend/bucket/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"path"
+	"strings"
+
+	Api_lib "github.com/Visoff/idk_some_name/golang_library/api/lib"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	Api_lib.Rest(mux, "/").Get(func(w http.ResponseWriter, r *http.Request) {
+		res := Api_lib.NewBetterResponseWriter(w)
+		file, err := os.ReadFile(path.Join("./static/", r.URL.Path))
+		if err != nil {
+			res.Status(404).Send("File not found")
+			return
+		}
+		res.Status(200).Send(file)
+	}).Post(func(w http.ResponseWriter, r *http.Request) {
+		res := Api_lib.NewBetterResponseWriter(w)
+		req := Api_lib.NewBetterRequest(r)
+		url_path := strings.Split(r.URL.Path, "/")
+		err := os.MkdirAll(path.Join("./static/", strings.Join(url_path[:len(url_path)-1], "/")), os.ModePerm)
+		if err != nil {
+			res.Status(500).Send(err.Error())
+			return
+		}
+		data := req.Body()
+		err = os.WriteFile(path.Join("./static/", url_path[len(url_path)-1]), data, os.ModePerm)
+		if err != nil {
+			res.Status(500).Send(err.Error())
+			return
+		}
+		res.Status(200).Send(data)
+	}).Delete(func(w http.ResponseWriter, r *http.Request) {
+		res := Api_lib.NewBetterResponseWriter(w)
+		err := os.Remove(path.Join("./static/", r.URL.Path))
+		if err != nil {
+			res.Status(500).Send(err.Error())
+			return
+		}
+		res.Status(200).Send("ok")
+	}).Apply()
+	http.ListenAndServe(":8080", mux)
+}
