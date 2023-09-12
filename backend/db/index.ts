9cf21af5c92be9b7bc2913e5369aa6327120a23d
Ilya <ikalinin01@mail.ru>
2023-09-12 20:35:31 +0600

    fake time database

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -1,6 +1,4 @@
 import express from 'express'
-import graphql from 'graphql'
-import express_graphql from 'express-graphql'
 import pg from 'pg'
 
 const db = new pg.Client({
@@ -15,6 +13,39 @@ db.connect().then(() => {console.log("db connected")})
 const app = express();
 
 app.use(express.json())
+
+const subscribers = new Map()
+
+function sendEvent(event:{type:"insert", table:string, row:string}|{type:"update", table:string, rows:string[]}|{type:"delete", table:string}) {
+    if (!subscribers.has(event.table)) {return}
+    subscribers.get(event.table).forEach(res => {
+        res.write(`event:${event.type}\ndata:${JSON.stringify(event)}\n\n`)
+        res.flushHeaders()
+    });
+}
+
+app.get("/:table/subscribe", (req, res) => {
+    const {table} = req.params
+
+    res.setHeader("Content-Type", "text/event-stream")
+    res.setHeader("Connection", "keep-alive")
+    res.setHeader("Cache-Control", "no-cache")
+
+    res.flushHeaders()
+
+    if (!subscribers.has(table)) {
+        subscribers.set(table, [])
+    }
+
+    subscribers.get(table).push(res)
+
+    req.on("close", () => {
+        subscribers.set(
+            table, 
+            subscribers.get(table).filter(el => res !== el)
+        )
+    })
+})
 
 app.route("/:table")
 .get((req, res) => {
@@ -36,6 +67,11 @@ app.route("/:table")
     const columns = Object.keys(body)
 
     db.query(`insert into "${table}"(${columns.map(el => `"${el}"`).join(", ")}) values(${columns.map(key => `'${body[key]}'`).join(", ")}) returning *`).then(result => {
+        sendEvent({
+            "row":result.rows[0],
+            "table":table,
+            "type":"insert"
+        })
         res.status(200).send(result.rows)
     }).catch(err => {
         res.status(500).send(err)
@@ -50,7 +86,12 @@ app.route("/:table")
     const columns = Object.keys(body)
 
     db.query(`update "${table}" set ${columns.map(key => `"${key}" = '${body[
-        key]}'`).join(", ")} ${query};`).then(result => {
+        key]}'`).join(", ")} ${query} returning *`).then(result => {
+            sendEvent({
+                "rows":result.rows,
+                "table":table,
+                "type":"update"
+            })
         res.status(200).send(result.rows)
     }).catch(err => {
         res.status(500).send(err)
@@ -62,8 +103,12 @@ app.route("/:table")
     delete(req.query.select)
     const query = Object.keys(req_query).map(key => `${key} ${req_query[key]}`).join(" ")
 
-    db.query(`delete from "${table}" ${query};`).then(result => {
+    db.query(`delete from "${table}" ${query} returning *`).then(result => {
         res.status(200).send(result.rows)
+        sendEvent({
+            "type":"delete",
+            "table":table
+        })
     }).catch(err => {
         res.status(500).send(err)
     })
