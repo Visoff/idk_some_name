050565b6c9c1c38e8388daf067e5499bee4fb82b
Ilya <ikalinin01@mail.ru>
2023-09-18 18:22:43 +0600

    astro configuration

--- astro_client/astro.config.mjs
+++ astro_client/astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
-import svelte from "@astrojs/svelte";
 
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node"
 
 // https://astro.build/config
 export default defineConfig({
+  output:"hybrid",
+  adapter:node({
+    mode:"standalone"
+  }),
+  server:{
+    host:"0.0.0.0",
+    port:3000
+  },
   integrations: [svelte(), tailwind()]
 });
