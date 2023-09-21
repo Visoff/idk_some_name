9082b1c7fcc3f8267d9bd9c27ac2e36f9615e23f
Ilya <ikalinin01@mail.ru>
2023-09-21 12:27:55 +0600

    ...

---
+++ astro_client/src/app/AppNavLayout.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    let navOpened:boolean = false
+</script>
+
+<main class="
+    absolute top-0 left-0 w-screen h-screen
+    flex
+">
+    <nav class="
+        h-screen w-screen md:w-auto
+        md:flex-1
+        max-md:fixed top-0 left-0 transition-transform {navOpened ? "" : "max-md:-translate-x-full"}
+        bg-blue-200
+    ">
+        <nav class="
+            md:hidden
+            w-full h-12
+            p-2
+        ">
+            <button class="
+                h-full aspect-square
+            "
+            on:click={() => {navOpened = !navOpened}}
+            >=</button>
+        </nav>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+        </ul>
+    </nav>
+    <div class="
+        flex-[6]
+        flex flex-col
+        bg-blue-200
+    ">
+        <nav class="
+            md:hidden
+            w-full h-12
+            bg-red-200
+            p-2
+        ">
+            <button class="
+                h-full aspect-square
+            "
+            on:click={() => {navOpened = !navOpened}}
+            >=</button>
+        </nav>
+        <slot/>
+    </div>
+</main>
